07778a4af0af0c9bc30e47827b257e5ce0fedf0b
bhaskar.guthula <bhaskar.guthula@aadhan.in>
2023-10-26 12:39:28 +0530

    starter remix added

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import type { LinksFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
@@ -8,50 +7,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useRouteError
 } from "@remix-run/react";
-import tailwindCssStyleSheet from "~/tailwind.css";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import aadhanIcon from "../public/favicon.svg";
 
 export const links: LinksFunction = () => [
-  { rel: "icon", href: aadhanIcon },
-  {
-    rel: "preconnect",
-    href: "https://fonts.googleapis.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
-  },
-  { rel: "stylesheet", href: tailwindCssStyleSheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export default function App() {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 2000,
-          },
-        },
-      }),
-  )
-
-
   return (
     <html lang="en">
       <head>
@@ -61,31 +23,10 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <QueryClientProvider client={queryClient}>
-          <Outlet />
-        </QueryClientProvider>
-        <LiveReload />
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export function ErrorBoundary() {
-  const error = useRouteError();
-  console.error(error);
-  return (
-    <html>
-      <head>
-        <title>Oh no!</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {JSON.stringify(error)}
-        {/* add the UI you want your users to see */}
-        <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
